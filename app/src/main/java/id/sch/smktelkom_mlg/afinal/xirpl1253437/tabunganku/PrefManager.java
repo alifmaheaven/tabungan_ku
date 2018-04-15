2123fe4e475d13ed7fe254b45f60a9883967be61
amandaprastika <thesa_prastika_25rpl@student.smktelkom-mlg.sch.id>
2018-04-15 11:33:12 +0700

    memodifikasi tombol lanjut dan lewati juga lingkarang kecil yang tampil pada slider (activity_welcome.xml)

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/PrefManager.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/PrefManager.java
@@ -1,8 +1,32 @@
 package id.sch.smktelkom_mlg.afinal.xirpl1253437.tabunganku;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Yhess on 4/15/2018.
  */
 
 public class PrefManager {
+    private static final String PREF_NAME = "introslider";
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+    int PRIVATE_MODE = 0;
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
 }
