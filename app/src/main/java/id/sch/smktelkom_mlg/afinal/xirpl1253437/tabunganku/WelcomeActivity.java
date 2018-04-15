8b3291a1114d61e630326cd462bde65e08cad8ae
amandaprastika <thesa_prastika_25rpl@student.smktelkom-mlg.sch.id>
2018-04-15 11:59:42 +0700

    pengaturan slider, event click button, notification bar dimasukan. (WelcomeActivity.java)

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/WelcomeActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/WelcomeActivity.java
@@ -1,13 +1,37 @@
 package id.sch.smktelkom_mlg.afinal.xirpl1253437.tabunganku;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnNext, btnSkip;
+    private PrefManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        prefManager = new PrefManager(this);
+        if (!prefManager.isFirstTimeLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
+
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_welcome);
     }
 }
