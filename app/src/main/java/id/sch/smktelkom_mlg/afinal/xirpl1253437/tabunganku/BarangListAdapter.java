fc1d09d15ebdfd7a947267f83cf47cf56db2c090
Lintangprtiwi <lintangprtiwi@gmail.com>
2018-04-27 13:50:31 +0700

    (+)class BarangListAdapter

---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/BarangListAdapter.java
@@ -0,0 +1,108 @@
+package id.sch.smktelkom_mlg.afinal.xirpl1253437.tabunganku;
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarangListAdapter extends RecyclerView.Adapter<BarangListAdapter.MemberViewHolder> {
+
+    private List<Barang> barangList;
+
+    public BarangListAdapter() {
+        barangList = new ArrayList<>();
+    }
+
+    private void add(Barang item) {
+        barangList.add(item);
+        notifyItemInserted(barangList.size() - 1);
+    }
+
+    public void addAll(List<Barang> barangList) {
+        for (Barang barang : barangList) {
+            add(barang);
+        }
+    }
+
+    public void remove(Barang item) {
+        int position = barangList.indexOf(item);
+        if (position > -1) {
+            barangList.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public void clear() {
+        while (getItemCount() > 0) {
+            remove(getItem(0));
+        }
+    }
+
+    public Barang getItem(int position) {
+        return barangList.get(position);
+    }
+
+
+    @Override
+    public MemberViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        MemberViewHolder memberViewHolder = new MemberViewHolder(view);
+        return memberViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final MemberViewHolder holder, int position) {
+        final Barang barang = barangList.get(position);
+
+        // holder.memberThumb.setImageResource(travel.getThumb());
+        holder.memberNama.setText(barang.getNama());
+        holder.memberHarga.setText(barang.getHarga());
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                String nama = holder.memberNama.getText().toString();
+                String harga = holder.memberHarga.getText().toString();
+                Intent i = new Intent(view.getContext(), Page1Activity.class);
+                i.putExtra("nm", nama);
+                i.putExtra("hg", harga);
+                view.getContext().startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return barangList.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+    }
+
+    public class MemberViewHolder extends RecyclerView.ViewHolder {
+        TextView memberNama;
+        TextView memberHarga;
+
+        public MemberViewHolder(final View itemView) {
+            super(itemView);
+            memberNama = itemView.findViewById(R.id.txtNm);
+            memberHarga = itemView.findViewById(R.id.txtHg);
+        }
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/ListAdapter.java
+++
@@ -1,4 +0,0 @@
-package id.sch.smktelkom_mlg.afinal.xirpl1253437.tabunganku;
-
-public class ListAdapter {
-}
