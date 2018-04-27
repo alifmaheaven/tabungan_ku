9fc120180486beec818b4a7e794687746efe04a6
Lintangprtiwi <lintangprtiwi@gmail.com>
2018-04-27 13:37:32 +0700

    (+)class Barang

---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/Barang.java
@@ -0,0 +1,30 @@
+package id.sch.smktelkom_mlg.afinal.xirpl1253437.tabunganku;
+
+public class Barang {
+
+    private String nama;
+    private String harga;
+
+    public Barang(String nama, String harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/ListAdapter.java
@@ -0,0 +1,4 @@
+package id.sch.smktelkom_mlg.afinal.xirpl1253437.tabunganku;
+
+public class ListAdapter {
+}
