c79cc3e2c83b0b5a5f569995a22c48b1b89fd24a
Lintangprtiwi <lintangprtiwi@gmail.com>
2018-04-29 13:16:02 +0700

    (+)Fragment Detail1 dan Detail2

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/PagerAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl1253437/tabunganku/PagerAdapter.java
@@ -1,13 +1,30 @@
 package id.sch.smktelkom_mlg.afinal.xirpl1253437.tabunganku;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 public class PagerAdapter extends FragmentStatePagerAdapter {
 
+    int mNoOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumberOfTabs) {
+        super(fm);
+        this.mNoOfTabs = NumberOfTabs;
+    }
+
     @Override
     public Fragment getItem(int position) {
-        return null;
+        switch (position) {
+            case 0:
+                Detail1 tab1 = new Detail1();
+                return tab1;
+            case 1:
+                Detail2 tab2 = new Detail2();
+                return tab2;
+            default:
+                return null;
+        }
     }
 
     @Override
